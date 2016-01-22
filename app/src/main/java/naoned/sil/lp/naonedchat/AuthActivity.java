38b869bdf62abe3d89705d316eb58e8db61af96f
ACHP <bullrot-10@hotmail.fr>
2016-01-22 08:26:49 +0100

    Add account manager auth system

---
+++ app/src/main/java/naoned/sil/lp/naonedchat/AuthActivity.java
@@ -0,0 +1,135 @@
+package naoned.sil.lp.naonedchat;
+/**
+ * Created by ACHP on 21/01/2016.
+ */
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+
+public class AuthActivity extends Activity {
+
+    private static final int AUTHORIZATION_CODE = 1993;
+    private static final int ACCOUNT_CODE = 1601;
+
+    private AuthPreferences authPreferences;
+    private AccountManager accountManager;
+
+    /**
+     * change this depending on the scope needed for the things you do in
+     * doCoolAuthenticatedStuff()
+     */
+    private final String SCOPE = "https://www.googleapis.com/auth/googletalk";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        accountManager = AccountManager.get(this);
+
+        authPreferences = new AuthPreferences(this);
+        if (authPreferences.getUser() != null
+                && authPreferences.getToken() != null) {
+            doCoolAuthenticatedStuff();
+        } else {
+            chooseAccount();
+        }
+    }
+
+    private void doCoolAuthenticatedStuff() {
+        // insert cool stuff with authPreferences.getToken()
+
+        Log.e("AuthApp", authPreferences.getToken());
+        Log.e("USER", authPreferences.getUser());
+        Intent intent = new Intent(AuthActivity.this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+
+    private void chooseAccount() {
+        // use https://github.com/frakbot/Android-AccountChooser for
+        // compatibility with older devices
+        Intent intent = AccountManager.newChooseAccountIntent(null, null,
+                new String[] { "com.google" }, false, null, null, null, null);
+        startActivityForResult(intent, ACCOUNT_CODE);
+    }
+
+    private void requestToken() {
+        Account userAccount = null;
+        String user = authPreferences.getUser();
+        for (Account account : accountManager.getAccountsByType("com.google")) {
+            if (account.name.equals(user)) {
+                userAccount = account;
+
+                break;
+            }
+        }
+
+        accountManager.getAuthToken(userAccount, "oauth2:" + SCOPE, null, this,
+                new OnTokenAcquired(), null);
+    }
+
+    /**
+     * call this method if your token expired, or you want to request a new
+     * token for whatever reason. call requestToken() again afterwards in order
+     * to get a new token.
+     */
+    private void invalidateToken() {
+        AccountManager accountManager = AccountManager.get(this);
+        accountManager.invalidateAuthToken("com.google",
+                authPreferences.getToken());
+
+        authPreferences.setToken(null);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == AUTHORIZATION_CODE) {
+                requestToken();
+            } else if (requestCode == ACCOUNT_CODE) {
+                String accountName = data
+                        .getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                authPreferences.setUser(accountName);
+
+                // invalidate old tokens which might be cached. we want a fresh
+                // one, which is guaranteed to work
+                invalidateToken();
+
+                requestToken();
+            }
+        }
+    }
+
+    private class OnTokenAcquired implements AccountManagerCallback<Bundle> {
+
+        @Override
+        public void run(AccountManagerFuture<Bundle> result) {
+            try {
+                Bundle bundle = result.getResult();
+
+                Intent launch = (Intent) bundle.get(AccountManager.KEY_INTENT);
+                if (launch != null) {
+                    startActivityForResult(launch, AUTHORIZATION_CODE);
+                } else {
+                    String token = bundle
+                            .getString(AccountManager.KEY_AUTHTOKEN);
+
+                    authPreferences.setToken(token);
+
+                    doCoolAuthenticatedStuff();
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
