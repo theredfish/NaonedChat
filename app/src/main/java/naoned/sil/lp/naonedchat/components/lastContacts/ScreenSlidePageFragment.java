b164ce5b1e8952c2c95f8d7f74dcfe81ae2bcb6b
ACHP <bullrot-10@hotmail.fr>
2016-01-27 00:06:14 +0100

    modif layout contact

--- app/src/main/java/naoned/sil/lp/naonedchat/components/lastContacts/ScreenSlidePageFragment.java
+++ app/src/main/java/naoned/sil/lp/naonedchat/components/lastContacts/ScreenSlidePageFragment.java
@@ -1,10 +1,13 @@
 package naoned.sil.lp.naonedchat.components.lastContacts;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.jivesoftware.smackx.vcardtemp.packet.VCard;
@@ -15,6 +18,7 @@ import naoned.sil.lp.naonedchat.R;
  * Created by ACHP on 24/01/2016.
  */
 public class ScreenSlidePageFragment extends Fragment {
+    //TODO : étendre VCard pour avoir plus d'attributs ? ( notifs ?)
     private VCard contact;
 
     public void setObject(VCard msg){
@@ -23,8 +27,13 @@ public class ScreenSlidePageFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_screen_slide_page, container, false);
-       // ((TextView)rootView.findViewById(R.id.monTextModifiable)).setText(this.contact.toString());
+        ((TextView)rootView.findViewById(R.id.lastContactsNomContacts)).setText(this.contact.getFirstName() + " " + this.contact.getLastName());
 
+        byte[] byteArray = this.contact.getAvatar();
+        Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        ImageView image = (ImageView) rootView.findViewById(R.id.lastContactsPhoto);
+
+        image.setImageBitmap(bmp);
         return rootView;
     }
 }
